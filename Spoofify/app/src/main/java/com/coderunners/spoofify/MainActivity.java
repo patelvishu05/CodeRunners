5621c63870416d64d09f5f2d6a7a82494634a2dd
patelvishu05 <35935851+patelvishu05@users.noreply.github.com>
2018-10-29 18:10:21 -0500

    Initialized Spoofify

---
+++ Spoofify/app/src/main/java/com/coderunners/spoofify/MainActivity.java
@@ -0,0 +1,12 @@
+package com.coderunners.spoofify;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
