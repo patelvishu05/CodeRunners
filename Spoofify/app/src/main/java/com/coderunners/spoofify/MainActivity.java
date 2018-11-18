c08fedd45e10e9a9c69eefa02a876129ea1da543
patelvishu05 <35935851+patelvishu05@users.noreply.github.com>
2018-11-18 17:05:07 -0600

    music player initial

--- Spoofify/app/src/main/java/com/coderunners/spoofify/MainActivity.java
+++ Spoofify/app/src/main/java/com/coderunners/spoofify/MainActivity.java
@@ -1,13 +1,49 @@
 package com.coderunners.spoofify;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mySong;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_song_player);
+        mySong = MediaPlayer.create(MainActivity.this, R.raw.chainsmokers);
+    }
+
+    public void playIt(View v)
+    {
+        mySong.start();
+    }
+
+    public void pauseIt(View v)
+    {
+        mySong.stop();
+    }
+
+    public void prevSongIt(View v)
+    {
+        ArrayList<Integer> playlist = new ArrayList<>();
+
+
+        playlist.add(R.raw.dura);
+        mySong.stop();
+        mySong = MediaPlayer.create(this, playlist.get(0));
+        mySong.start();
+    }
+
+    public void skipIt(View v)
+    {
+        mySong.stop();
+        mySong = MediaPlayer.create(MainActivity.this, R.raw.dura);
+        mySong.start();
     }
 }
